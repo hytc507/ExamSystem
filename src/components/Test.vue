b9e7a2c4215fa87e5dd5cd7f5f6767906d76d998
fengying <765843398@qq.com>
2017-12-05 17:55:31 +0800

    build vue

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    {{msg}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Test',
+    data () {
+      return {
+        msg: '2333'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    color: #666666;
+  }
+</style>
